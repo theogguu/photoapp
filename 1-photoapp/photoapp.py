db4abf89413b172e09a4a23dfd36924a0dc11b3c
theogguu <stevengu.is.not@gmail.com>
2023-04-14 16:01:40 -0500

    Update photoapp.py

--- 1-photoapp/photoapp.py
+++ 1-photoapp/photoapp.py
@@ -1,2 +1,26 @@
+import pymysql
+import sys
 import uuid
-print(uuid.uuid4())
+# print(uuid.uuid4())
+
+ENDPOINT = "photoapp-steven-gu.s3.us-east-2.amazonaws.com"
+USER = "admin"
+PASSWORD = "1haveasecretpassword"
+DBNAME = "sys" # sys is the default database #suggested "photoapp"
+
+print('starting...')
+try:
+    dbconn = pymysql.connect(ENDPOINT, USER, PASSWORD, DBNAME)
+    cursor = dbconn.cursor()
+    cursor.execute("SELECT now()")
+    query_results = cursor.fetchall()
+    print(query_results)
+
+except Exception as e:
+    print("Database connection failed due to {}".format(e))
+    sys.exit(-1)
+    
+cursor.close()
+dbconn.close()
+print()
+print('done')
